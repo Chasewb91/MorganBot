bbc7fd40578502217dd61d1726054e28c6cde737
Chase Boyd <chasewb91@gmail.com>
2021-02-13 03:00:35 +0000

    updates

--- cogs/dbcog.py
+++ cogs/dbcog.py
@@ -5,7 +5,7 @@ from tinydb.operations import increment, decrement
 
 db = TinyDB('db.json')
 
-class Database(commands.Cog):
+class Scorecard(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
@@ -13,36 +13,36 @@ class Database(commands.Cog):
     async def register(self, ctx):
       user = ctx.message.author
       userdb = str(user)
-      db.insert({'User': userdb, 'inspiration': 0,})
-      await ctx.channel.send("You have registered for the inspiration counter!")
+      db.insert({'User': userdb, 'Points': 0,})
+      await ctx.channel.send("You have registered for the points system!")
 
 
     @commands.command()
     async def query(self, ctx):
       user = ctx.author
       userdb = str(user)
-      Inspo = Query()
-      tally  = db.search(Inspo.User == userdb)
+      Points = Query()
+      tally  = db.search(Points.User == userdb)
       await ctx.channel.send(tally)
 
     @commands.command()
-    async def inspo(self,ctx, user: discord.User):
+    async def point(self,ctx, user: discord.User):
       userdb = str(user)
       User = Query()
-      db.update(increment('inspiration'), User.User == userdb)
-      print(userdb + " has gained inspiration!")
-      await ctx.channel.send("Inspiration has been awarded!")
+      db.update(increment('Points'), User.User == userdb)
+      print(userdb + " has gained a point!")
+      await ctx.channel.send(f"A point has been awarded to {userdb}!")
 
     @commands.command()
-    async def dinspo(self, ctx, user: discord.User):
+    async def unpoint(self, ctx, user: discord.User):
       userdb = str(user)
       User = Query()
-      db.update(decrement('inspiration'), User.User == userdb)
-      print(userdb + " has lost inspiration!")
-      await ctx.channel.send("Inspiration has been used!")     
+      db.update(decrement('Points'), User.User == userdb)
+      print(userdb + " has lost a point!")
+      await ctx.channel.send(f"Point taken from {userdb}!")     
 
 
 
 def setup(bot):
-    bot.add_cog(Database(bot))
+    bot.add_cog(Scorecard(bot))
 
